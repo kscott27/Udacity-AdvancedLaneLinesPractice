84a2b658d07fdf8326ea12a9634914269cb04e6e
kscott27 <kmscott27@gmail.com>
2018-11-10 20:47:49 -0800

    Added pipeline function

---
+++ Pipeline.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from GradientHelpers import dir_threshold
+
+
+image = mpimg.imread('../images/bridge_shadow.jpg')
+
+# Edit this function to create your own pipeline.
+def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    l_channel = hls[:,:,1]
+    s_channel = hls[:,:,2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+    
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+
+    dir_binary = dir_threshold(image, sobel_kernel=7, thresh=(0.7, 1.3))
+
+    # Combine the two binary thresholds
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[((s_binary == 1) | (sxbinary == 1)) & (dir_binary == 1)] = 1
+
+    return combined_binary, color_binary
+    
+combined_binary, color_binary = pipeline(image)
+
+# Plot the result
+f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 9))
+f.tight_layout()
+
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=40)
+
+ax2.imshow(combined_binary)
+ax2.set_title('Combined', fontsize=40)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+ax3.imshow(color_binary)
+ax3.set_title('Color', fontsize=40)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+plt.savefig('../images/pipeline_output.jpg')
