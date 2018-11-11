007831a6e861c2e276e72fe2be255ddebbea2385
kscott27 <kmscott27@gmail.com>
2018-11-10 18:40:50 -0800

    Finished the various thresholding functions.

---
+++ CombiningThresholds.py
@@ -0,0 +1,8 @@
+# Choose a Sobel kernel size
+ksize = 3 # Choose a larger odd number to smooth gradient measurements
+
+# Apply each of the thresholding functions
+gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
